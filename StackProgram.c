f27400db3de4c12e044155075c40dc2688f191c2
anilgardd <98427173+anilgardd@users.noreply.github.com>
2022-10-07 00:22:38 +0300

    Add files via upload

---
+++ StackProgram.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int count = 0;
+
+// Creating a stack
+struct stack {
+  int items[MAX];
+  int top;
+};
+typedef struct stack st;
+
+void createEmptyStack(st *s) { s->top = -1; }
+
+// Check if the stack is full
+int isfull(st *s) {
+  if (s->top == MAX - 1)
+    return 1;
+  else
+    return 0;
+}
+
+// Check if the stack is empty
+int isempty(st *s) {
+  if (s->top == -1)
+    return 1;
+  else
+    return 0;
+}
+
+// Add elements into stack
+void push(st *s, int newitem) {
+  if (isfull(s)) {
+    printf("STACK FULL");
+  } else {
+    s->top++;
+    s->items[s->top] = newitem;
+  }
+  count++;
+}
+
+// Remove element from stack
+void pop(st *s) {
+  if (isempty(s)) {
+    printf("\n STACK EMPTY \n");
+  } else {
+    printf("Item popped= %d", s->items[s->top]);
+    s->top--;
+  }
+  count--;
+  printf("\n");
+}
+
+// Print elements of stack
+void printStack(st *s) {
+  printf("Stack: ");
+  for (int i = 0; i < count; i++) {
+    printf("%d ", s->items[i]);
+  }
+  printf("\n");
+}
+
+// Search items in stack
+void search(st *s, int searchedItem) {
+  for (int i = 0; i < MAX; i++) {
+    if (searchedItem == s->items[i]) {
+      printf("%d is found  ", s->items[i]);
+      printf("at location %d (from the bottom). \n", MAX - i);
+    }
+  }
+}
+
+// Driver code
+int main() {
+  int ch;
+  st *s = (st *)malloc(sizeof(st));
+  int max = 100;
+  int random;
+  createEmptyStack(s);
+  while (max--) {
+    random = rand() % 1000 + 1;
+    push(s, random);
+  }
+  search(s, 887);
+
+  printStack(s);
+}
